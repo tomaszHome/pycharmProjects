8bf347e5a10e353d567c6a6fe247cdbca0c484cc
tomaszhub <tomasz.chanusiak@spire.com>
2018-02-11 14:00:50 +0000

    WIP

--- csv_formating/main.py
+++ csv_formating/main.py
@@ -1,10 +1,10 @@
 import csv
-from collections import Counter
 
+# Stores organised parts
 parts = {}
-numberList = {}
 
 with open('csv_test_invertory.csv', mode='rU') as csvFile:
+    # Reads csv file, delimiter set to ','
     reader = csv.reader(csvFile, delimiter=',')
     for n, row in enumerate(reader):
         if not n:
@@ -20,7 +20,25 @@ with open('csv_test_invertory.csv', mode='rU') as csvFile:
 
     for key, value in parts.items():
         # print value
-        print(key, len([item for item in value if item]))
+        counter = key, len([item for item in value if item])
+        print(counter)
+
+    report = 'report.csv'
+    csv = open(report, "w")
+    columnTitleRow = "part name, part number\n"
+
+    csv.write(columnTitleRow)
+
+    for part in parts:
+        #print(parts.keys())
+        partname = part
+        print(partname)
+        partnumber = parts[part]
+        for items in partnumber:
+            print(items)
+            row = str(partname) + "," + str(items) + "\n"
+            csv.write(row)
+
 
 
 
