877c672acdbd146ada92a9b2b4cdcead6126eedc
tomaszhub <tomasz.chanusiak@spire.com>
2018-03-06 13:28:01 +0000

    WIP

--- py_test/mazkanata.py
+++ py_test/mazkanata.py
@@ -2,16 +2,18 @@ import pytest
 
 x_list = [1, 2, 3]
 x_dict = {'key_1': 10, 'key_2': 20, 'key_3': 30}
-x_value = None
+x_value = 2
+# number = 5
 
 
 # content of mazkanata.py
 class TestClass(object):
 
-    @pytest.fixture(scope="session")
+    @pytest.fixture(scope="class")
     def number(self):
         assert x_value is not None, "Wrong input"
         return x_value
+
 
     def test_one(self):
         x = "this"
@@ -20,5 +22,11 @@ class TestClass(object):
     def test_two(self):
         assert x_list[0] == 1, "Wrong number!"
 
-    def test_three(self, number):
+
+
+    @pytest.mark.usefixtures("number")
+    def test_three(self, request):
+        def print_info():
+            print "teardown"
+        request.addfinalizer(lambda: print_info)
         assert number == 5
